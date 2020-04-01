e1c9d9c846ad15153e599426fcef595168192c94
retrongames <62163238+retrongames@users.noreply.github.com>
2020-03-31 22:30:33 -0300

    Add files via upload

---
+++ scriptzinho.js
@@ -0,0 +1,29 @@
+let tela_inicio = document.getElementById('tela-inicio');
+let tela_menu = document.getElementById('tela-menu');
+let tela_tutorial = document.getElementById('colocar-aqui-a-id-da-tela-tutorial');
+let tela_comando = document.getElementById('colocar-aqui-a-id-da-tela-comando');
+let tela_jogar = document.getElementById('tela-jogar');
+
+setTimeout(function splash_screen() {
+  document.querySelector('div.tela.active').classList.remove('active')
+  tela_menu.classList.add('active')
+  console.log(location.pathname)
+}, 3000)
+
+function tutorial() {
+  document.querySelector('div.tela.active').classList.remove('active')
+  tela_tutorial.classList.add('active')
+  console.log(location.pathname)
+}
+
+function comando() {
+  document.querySelector('div.tela.active').classList.remove('active')
+  tela_comando.classList.add('active')
+  console.log(location.pathname)
+}
+
+function jogar() {
+  document.querySelector('div.tela.active').classList.remove('active')
+  tela_jogar.classList.add('active')
+  console.log(location.pathname)
+}
